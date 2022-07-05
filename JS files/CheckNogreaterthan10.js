229f71762e94dae54ef435271443caed24af7635
mariselvan21 <mari20041999@gmail.com>
2022-07-05 16:14:23 +0530

    add files

---
+++ JS files/CheckNogreaterthan10.js
@@ -0,0 +1,13 @@
+var x=2;
+if(x>10)
+{
+    console.log("This number is greater than 10:",x);
+}
+else if(x=10)
+{
+    console.log("This number is equal to 10:",x);
+}
+else
+{
+    console.log("The number is less than 10:",x);
+}
---
+++ JS files/NoEvenorOdd.js
@@ -0,0 +1,9 @@
+var x=9;
+var y=x%2;
+if(y=0)
+{
+    console.log("This number is even:",x);
+}
+else{
+    console.log("This number is odd:",x);
+}
