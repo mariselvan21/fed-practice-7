fcffd5b8245a0a65c4938c91a24f37310b5441be
mariselvan21 <mari20041999@gmail.com>
2022-07-06 11:28:08 +0530

    all files

---
+++ JS files/First_N_Even_Numbers.js
@@ -0,0 +1,10 @@
+var i;
+console.log("First N even numbers");
+for(i=1;i<=20;i++)
+{
+    x=i%2;
+    if(x==0)
+    {
+    console.log(i);
+    }
+}
---
+++ JS files/First_N_Odd_Numbers.js
@@ -0,0 +1,10 @@
+var i;
+console.log("First N odd numbers:");
+for(i=1;i<=20;i++)
+{
+    x=i%2;
+    if(x!==0)
+    {
+        console.log(i);
+    }
+}
