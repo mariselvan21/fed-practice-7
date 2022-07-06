1bf87f005048f112e68b5cfe0ac1713c43803522
mariselvan21 <mari20041999@gmail.com>
2022-07-06 20:40:33 +0530

    add files

---
+++ JS files/Factorial_of_number.js
@@ -0,0 +1,9 @@
+var i;
+var num=3;
+
+for(i=num-1;i>=1;i--)
+(
+    num*=i
+    
+)
+console.log("Factorial of number:"+num);
--- JS files/Sum_of_N_natural_numbers.js
+++ JS files/Sum_of_N_natural_numbers.js
@@ -1,3 +1,8 @@
-var n=10;
-var sum=(n*(n+1))/2;
-console.log("sum of first "+n +" natural numbers:"+sum);
+var i;
+var sum=0;
+/**var sum=(n*(n+1))/2;**/
+for (i=1;i<=10;i++)
+{
+    sum+=i;
+}
+console.log("sum of first 10 natural numbers:"+sum);
