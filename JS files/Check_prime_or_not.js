f100d57f4e65ee16a1a50ab8f3904a5b06808bd7
mariselvan21 <mari20041999@gmail.com>
2022-07-06 23:12:43 +0530

    add files

---
+++ JS files/Check_prime_or_not.js
@@ -0,0 +1,21 @@
+var num=10;
+var x,y,z;
+x=num%2;
+y=num%3;
+z=num%5;
+if(x==0)
+{
+    console.log(num+" is not prime number.");
+}
+else if(y==0)
+{
+    console.log(num+" is not prime number.");
+}
+else if(z==0)
+{
+    console.log(num+" is not prime number.");
+}
+else
+{
+    console.log(num+" is prime number.");
+}
