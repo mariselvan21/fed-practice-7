4bf3683b0df309350762359d8bb04bfc1c9fa6bd
mariselvan21 <mari20041999@gmail.com>
2022-07-08 12:23:25 +0530

    add files

---
+++ JS files/Check_no._Armstrong_or_not.js
@@ -0,0 +1,35 @@
+var number=prompt("Enter any vale:");
+var temp=number;
+var flag=0;
+var lastDigit;
+
+while(temp>=1)
+(
+    flag=flag+1;
+    lastDigit=temp%10;
+    temp=temp-lastDigit;
+    temp=temp/10;
+)
+temp=number;
+sum=0;
+while(temp>=1)
+{
+    lastDigit=temp%10;
+    temp=temp-lastDigit;
+    temp=temp/10;
+    var multiplicationValue=1;
+    for(i=1;i<=flag;i++)
+    {
+        var multiplicationValue=multiplicationValue*lastDigit;
+
+    }
+    sum=sum+multiplicationValue;
+
+}
+if(sum==number)
+{
+    console.log(number+" is a armstrong number.")
+}
+else{
+    console.log(number+" is not a armstrong number.")
+}
